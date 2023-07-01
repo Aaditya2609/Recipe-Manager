32ae76cb9896cef394e58496de5e4c5d2612f130
Aaditya Khantal <khantalak2609@gmail.com>
2023-07-01 23:05:04 +0530

    update sample image change

--- src/context/RecipeContext.jsx
+++ src/context/RecipeContext.jsx
@@ -9,7 +9,7 @@ const recipesData = [
     instructions:
       "Cook the spaghetti according to package instructions.\nIn a large pan, heat oil and sauté the onions and garlic until fragrant.\nAdd the ground beef and cook until browned.\nStir in the canned tomatoes, tomato paste, oregano, basil, salt, and pepper.\nSimmer the sauce for 15-20 minutes.\nServe the sauce over the cooked spaghetti.",
     cuisine: "Italian",
-    image: "https://picsum.photos/?random=8"
+    image: "https://res.cloudinary.com/dyicdo3vs/image/upload/v1688232780/samples/food/pot-mussels.jpg"
   },
   {
     name: "Chicken Curry",
@@ -18,7 +18,7 @@ const recipesData = [
     instructions:
       "Heat oil in a large pan and sauté the onions and garlic until fragrant.\nAdd the chicken thighs and cook until browned.\nAdd the curry powder, turmeric, cumin, salt, and pepper. Stir well.\nAdd the red bell pepper and cook for a few more minutes.\nPour in the coconut milk and simmer for 20-25 minutes.\nServe the chicken curry with rice or naan bread.",
     cuisine: "Indian",
-    image: "https://picsum.photos/?random=3"
+    image: "https://res.cloudinary.com/dyicdo3vs/image/upload/v1688232804/cld-sample-4.jpg"
   },
   {
     name: "Caesar Salad",
@@ -27,7 +27,7 @@ const recipesData = [
     instructions:
       "Preheat the oven to 180°C (350°F).\nTear the bread into small pieces and toss with olive oil and minced garlic.\nSpread the bread pieces on a baking sheet and bake for 10-15 minutes until crispy.\nIn a large bowl, combine grated Parmesan cheese, lemon juice, Worcestershire sauce, salt, and pepper.\nAdd the torn lettuce leaves and croutons to the bowl.\nToss everything together until well coated.\n7. Serve the Caesar salad as a side or main dish.",
     cuisine: "American",
-    image: "https://picsum.photos/?random=5"
+    image: "https://res.cloudinary.com/dyicdo3vs/image/upload/v1688232802/samples/breakfast.jpg"
   }
 ];
 
